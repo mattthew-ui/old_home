7c2651e5aefd3f97a5ed8df3eeda01b86c9c0a0f
Ailish <snickrrdoodles@gmail.com>
2025-12-09 14:49:31 -0500

    Payment page added to admin view

--- resources/views/Payment.blade.php
+++ resources/views/Payment.blade.php
@@ -4,6 +4,11 @@
     <title>Payment</title>
 </head>
 <style>
+    * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
     body{
         background: #3d2f1d;    
         font-family: "Georgia", serif;
@@ -13,6 +18,7 @@
             justify-content: center;
         }
     .paper {
+        margin-top: 160px;
         width: 700px;
         background: #f5e6c8;
         padding: 40px;
@@ -21,6 +27,46 @@
             0 0 40px 10px rgba(0,0,0,0.6),
             inset 0 0 50px rgba(0,0,0,0.4);
     }
+    header{
+    background-color: #f5e6c8;
+    border-bottom: 1px solid #3d2f1d;
+    width: 100%;
+    height: 120px;
+    position: fixed;
+    top: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 40px;
+
+    box-shadow: 0 0 40px 10px rgba(0,0,0,0.1),
+                0 0 50px rgba(0,0,0,0);
+    background-image:
+        radial-gradient(rgba(0,0,0,0.2) 1px, transparent 1px),
+        linear-gradient(0deg, #f1ddba, #f5e6c8);
+    background-size: 4px 4px, 100%;
+  }
+
+  header img{
+    height: 100px;
+  }
+
+  .header-buttons{
+    display: flex; 
+    gap: 15px;
+  }
+
+  .header-buttons button {
+      padding: 10px 15px;
+      background: brown;
+      color: white;
+      border: none;
+      cursor: pointer;
+      border-radius: 5px;
+      white-space: nowrap;
+  }
     label { font-weight: bold; display: block; margin-top: 15px; }
     input {
         width: 100%;
@@ -28,7 +74,6 @@
         margin-top: 5px;
     }
     button {
-        margin-top: 20px;
         padding: 10px 15px;
         background: brown;
         color: white;
@@ -47,6 +92,24 @@
     .msg { color: red; margin-top: 10px; } 
 </style>
 <body>
+
+    <header>
+    <img src="https://cdn.discordapp.com/attachments/1436340554010202163/1441101630060892191/d98a94ac-ea43-401a-94ed-0654418bd71f-removebg-preview.png?ex=6938f5ba&is=6937a43a&hm=7b8c0648168f91714c93e674c0e5c765e5f5a811b3066cedae4c5e2531ba1148">
+
+    <div class="header-buttons">
+        <button onclick ="window.location ='{{ route('roster.daily') }}'">Daily Roster</button>
+        <button onclick ="window.location ='{{ route('AdminApproval') }}'">Register Requests</button>
+        <button onclick ="window.location ='{{ route('RoleCreation') }}'">Role Creation</button>
+        <button onclick ="window.location ='{{ route('EmployeeList')}}'">Employee List</button>
+        <button onclick ="window.location ='{{ route('Additional_Info') }}'">Patient Info</button>
+        <button onclick ="window.location ='{{ route('roster.new') }}'">New Roster</button>
+        <button onclick ="window.location ='{{ route('doctor_appointment.create') }}'">Doctor Appt</button>
+        <button onclick ="window.location ='{{ route('admin_report') }}'">Admin Report</button>
+        <button onclick ="window.location ='{{ route('payment.index') }}'">Payment</button>
+        <button onclick ="window.location ='{{ route('home') }}'">LOG OUT</button>
+    </div>
+  </header>
+
     <div class="outer-wrap">
         <div class="paper">
             <h2 style="text-align:center;">Payment</h2>
